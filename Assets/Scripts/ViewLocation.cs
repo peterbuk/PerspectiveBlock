2f9c85552334a227f788596eb895356c703b2a21
Peter Buk <peterbuk8@gmail.com>
2015-12-22 17:11:11 -0700

    Added Vuforia and Cardboard APKs

---
+++ Assets/Scripts/ViewLocation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ViewLocation : MonoBehaviour {
+
+    public Text debugText;
+    public GameObject ARCamera;
+    public GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        debugText.text = "Camera Location: " + ARCamera.transform.position.ToString() + "\n" +
+            "Camera Rotation: " + ARCamera.transform.rotation.ToString() +
+            "\nDistance from target: " +
+            Vector3.Distance(target.transform.position, ARCamera.transform.position).ToString() +
+            "\nTarget location: " + target.transform.position.ToString();
+    }
+}
