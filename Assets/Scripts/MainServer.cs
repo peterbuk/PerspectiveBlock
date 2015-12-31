3774cce4ed10f8e9756118020cf4ff94a27b6b04
Peter Buk <peterbuk8@gmail.com>
2015-12-30 17:35:52 -0700

    Network sends messages back and forth
    We are now connected! Added a ClientTest for PC sending as well that
    doesn't spam vuforia errors

--- Assets/Scripts/MainServer.cs
+++ Assets/Scripts/MainServer.cs
@@ -2,48 +2,50 @@
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
 
 public class MainServer : MonoBehaviour {
 
-    int myReliableChannelId;
-    int socketId;
-    int socketPort = 12345;
-    int connectionId;
+    const int PORT = 12238;
+    const short LOCATION_MSG = 55;
 
     NetworkClient client;
 
+    public Text debugText;
+
     // Use this for initialization
     void Start ()
-    {/*
-        NetworkTransport.Init();
-        ConnectionConfig config = new ConnectionConfig();
-        myReliableChannelId = config.AddChannel(QosType.Reliable);
-        int maxConnections = 10;
-        HostTopology topology = new HostTopology(config, maxConnections);
+    {
 
-        socketId = NetworkTransport.AddHost(topology, socketPort);
-        Debug.Log("Socket open. SocketId is:" + socketId);*/
-
-
-        NetworkServer.Listen(socketPort);
+        NetworkServer.Listen(PORT);
         Debug.Log("Listening");
+        debugText.text += "Listening";
 
-        //client = new NetworkClient();
-        client = ClientScene.ConnectLocalServer();
-        client.RegisterHandler(MsgType.Connect, OnConnected);
-        client.RegisterHandler(55, DootDoot);
+        NetworkServer.RegisterHandler(MsgType.Connect, OnConnected);
+        NetworkServer.RegisterHandler(MsgType.Disconnect, OnDisconnected);
+        NetworkServer.RegisterHandler(LOCATION_MSG, OnLocation);
+
+        debugText.text = Network.player.ipAddress;
     }
 
     public void OnConnected(NetworkMessage netMsg)
     {
-        Debug.Log("FINALLY CONNECTED");
+        Debug.Log("Client has connected");
+        debugText.text += "Client has connected";
     }
 
-    public void DootDoot(NetworkMessage netMsg)
+    public void OnDisconnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Client has disconnected");
+        debugText.text += "Client has disconnected";
+    }
+
+
+    public void OnLocation(NetworkMessage netMsg)
     {
         var msg = netMsg.ReadMessage<StringMessage>();
-        Debug.Log("msg:" + msg.value);
-
+        Debug.Log("msg: " + msg.value);
+        debugText.text += msg.value + "\n";
     }
 	
 	// Update is called once per frame
@@ -51,21 +53,4 @@ public class MainServer : MonoBehaviour {
     {
         
 	}
-
-    public void Doot()
-    {
-        NetworkServer.SendToAll(55, new StringMessage("Doot"));
-    }
-
-    public void Connect()
-    {
-        byte error;
-        connectionId = NetworkTransport.Connect(socketId, "127.0.0.1", socketPort, 0, out error);
-
-        if (!error.Equals(NetworkError.Ok))
-        {
-            Debug.Log("failed to connect :(");
-        }
-
-    }
 }
--- Assets/Scripts/ViewClient.cs
+++ Assets/Scripts/ViewClient.cs
@@ -2,45 +2,79 @@
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
 
 public class ViewClient : MonoBehaviour {
 
-    int myReliableChannelId;
-    int socketId;
-    int socketPort = 12345;
-    int connectionId;
+    const int PORT = 12238;
+    const short LOCATION_MSG = 55;
+
+    string ip = "172.17.14.89";
 
     NetworkClient client;
+    bool connected = false;
+
+    public Text debugText;
+    public InputField ipAddress;
+
+    public GameObject target;
+    public GameObject ARCamera;
 
     // Use this for initialization
     void Start ()
     {
         client = new NetworkClient();
-        client.RegisterHandler(55, DootDoot);
+        client.RegisterHandler(LOCATION_MSG, LocationReply);
         client.RegisterHandler(MsgType.Connect, OnConnected);
-        client.Connect("127.0.0.1", socketPort);
+        client.RegisterHandler(MsgType.Error, OnError);
 
+        ipAddress.text = ip;
     }
 
-    public void DootDoot(NetworkMessage netMsg)
+    public void Connect()
     {
-        var msg = netMsg.ReadMessage<StringMessage>();
-        Debug.Log("msg" + msg.value);
-        
+        debugText.text = "Connecting to" + ipAddress.text;
+        client.Connect(ipAddress.text, PORT);
     }
 
     public void OnConnected(NetworkMessage netMsg)
     {
-        Debug.Log("FINALLY CONNECTED!!!");
+        debugText.text = "Connected to server";
+        connected = true;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    public void OnError(NetworkMessage netMsg)
     {
-	
+        debugText.text = "error";
+    }
+
+    public void LocationReply(NetworkMessage netMsg)
+    {
+        var msg = netMsg.ReadMessage<StringMessage>();
+        debugText.text = "reply" + msg.value;
+    }
+
+
+    float timer = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (connected)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                float distance = Vector3.Distance(target.transform.position, ARCamera.transform.position);
+                client.Send(LOCATION_MSG, new StringMessage(distance.ToString()));
+                timer = 3;
+            }
+
+        }
 	}
 
-    public void Connect()
+    public void Doot()
     {
+        if (connected)
+            client.Send(LOCATION_MSG, new StringMessage("Dootdoot"));
     }
 }
